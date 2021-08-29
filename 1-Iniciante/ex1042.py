c7d4128e76f86ee2d278f95e5f9e8e9a4f57d501
Pedro Henrique Pereira Almeida <pedro.6571almeida@gmail.com>
2021-08-29 00:32:01 -0300

    Sort Simples

--- 1-Iniciante/ex1042.py
+++ 1-Iniciante/ex1042.py
@@ -6,29 +6,13 @@ n3 = int(n3)
 
 #processamento
 digitado = [n1, n2, n3]
-crescente = [0, 0, 0]
-
-if n1 == n2 and n2 == n3:
-    crescente = [n1, n2, n3]
-else:
-    if n1 < n2 and n2 < n3:
-        crescente = [n1, n2, n3]
-    elif n1 < n2 and n1 < n3:
-        crescente = [n1, n3, n2]
-    elif n2 < n1 and n1 < n3:
-        crescente = [n2, n1, n3]
-    elif n2 < n1 and n2 < n3:
-        crescente = [n2, n3, n1]
-    elif n3 < n1 and n1 < n2:
-        crescente = [n3, n1, n2]
-    else:
-        crescente = [n3, n2, n1]
+crescente = sorted(digitado)
 
 #saída 
 for cont in range(0,3):
-    print(f"{crescente[cont]}")
+    print("{}".format(crescente[cont]))
 
 print("")
 
 for cont in range(0,3):
-    print(f"{digitado[cont]}")
+    print("{}".format(digitado[cont]))
